b6a3b030e3380bc3233caeb3d46ace8b3a151dcc
Nilton Dias <ntjdias@gmail.com>
2021-10-02 21:14:13 -0300

    Inserir movimentação

---
+++ cypress/integration/SrBarriga.spec.js
@@ -0,0 +1,101 @@
+/// <reference types="cypress" />
+
+describe('Desafio SrBarriga React - Casos de testes', () => {
+
+	before(() => {
+		cy.visit('http://barrigareact.wcaquino.me')
+
+		// Informar o com o email
+		cy.get("[data-test='email']").type('nilton.dias@email.com')
+
+		// Informar a senha
+		cy.get("[data-test='passwd']").type('1234')
+
+		// Clicar no botão entrar
+		cy.get('.btn').click()
+
+	})
+
+	it('Incluir uma conta com sucesso', () => {
+
+		// Clicar em configurações
+		cy.get('[data-test=menu-settings]').click()
+
+		// Clicar em contas
+		cy.contains('Contas').click()
+
+		// Preencher o nome da conta
+		cy.get('[data-test=nome]').type('Conta Bradesco')
+
+		// Clicar no botão para salvar a conta
+		cy.get('.btn').click()
+
+		// Assertiva verificando a mensagem de sucesso
+		cy.get('.toast-message').should('contain', 'Conta inserida com sucesso!')
+
+	})
+
+	it('Alterar conta com sucesso ', () => {
+
+		// Clicar no botão alterar conta
+		cy.get('.fa-edit').click()
+
+		// Digitar novo nome da conta
+		cy.get('[data-test=nome]').clear()
+		cy.get('[data-test=nome]').type('Conta Itau')
+
+
+		// Clicar no botão para salvar a conta
+		cy.get('.btn').click()
+
+		// Assertiva verificando a mensagem de sucesso
+		cy.get('.toast-message').should('contain', 'Conta atualizada com sucesso!')
+
+	})
+
+	it('Incluir conta repetida e valiar mensagem', () => {
+
+		// Clicar em configurações
+		cy.get('[data-test=menu-settings]').click()
+
+		// Clicar em contas
+		cy.contains('Contas').click()
+
+		// Preencher o nome da conta
+		cy.get('[data-test=nome]').type('Conta Itau')
+
+		// Clicar no botão para salvar a conta
+		cy.get('.btn').click()
+
+		// Assertiva verificando a mensagem de sucesso
+		cy.get('.toast-message').should('contain', '400')
+
+	})
+
+	it('Inserir movimentação com sucesso', () => {
+
+		// Clicar em movimentações
+		cy.get(':nth-child(2) > .nav-link > .fas').click()
+
+		// Descrição da movientação
+		cy.get('#descricao').type('Saldo inicial')
+
+		// Valor do movimento
+		cy.get('.col-4 > .form-control').type('1500')
+
+		// Interessado / envolvido
+		cy.get('#envolvido').type('Eu mesmo')
+
+		// Conta corrente ( Select )
+		//cy.xpath("//*[data-test=conta]/option[contains(.,'teste')]").click()
+
+		// CLicar no botão para trocar o tipo para receita
+		cy.get('.col-2 > .btn').click()
+
+		// Clicar no botão para salvar o movimento
+		cy.get('.btn-primary').click()
+
+
+	})
+
+})
---
+++ documents/README.md
@@ -0,0 +1,30 @@
+# Projeto - Testes E2E com Cypress
+## Desafio SrBarriga React
+
+### Site utilizado nos testes
+- http://barrigareact.wcaquino.me
+
+### Dados para acessar o site
+- Usuário: nilton.dias@email.com
+- Senha: 1234
+
+## Casos de testes
+1. Inserir conta - ok
+	Validar a mensagem
+
+2. Alterar conta - ok
+	Apenas validar a mensagem
+
+3. Inserir conta repetida - ok
+	Validar a mensagem de erro
+
+4. Inserir movimentação - ok
+	Lançar uma despesa
+
+5. Cálculo de saldo
+	Validar se o saldo da conta foi calculado corretamente
+
+6. Remover movimentação
+	Na tela de históricos remover a movimentação 
+	e validar a mensagem
+
