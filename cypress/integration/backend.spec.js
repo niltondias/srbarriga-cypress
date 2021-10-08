9a06d1828c6273b42afcbc0d3aa470a81932f323
Nilton Dias <ntjdias@gmail.com>
2021-10-08 15:46:12 -0300

    Calcular saldo da conta via rest

--- cypress/integration/backend.spec.js
+++ cypress/integration/backend.spec.js
@@ -86,9 +86,7 @@ describe('Desafio SrBarriga React - Testes funcionais', () => {
 			cy.request({
 				method: 'POST',
 				url: '/transacoes',
-				headers: {
-					Authorization: `JWT ${token}`
-				},
+				headers: { Authorization: `JWT ${token}` },
 				body: {
 					conta_id: contaId,
 					data_pagamento: Cypress.moment().add({ days: 1 }).format('DD/MM/YYYY'),
@@ -108,12 +106,61 @@ describe('Desafio SrBarriga React - Testes funcionais', () => {
 
 	})
 
-	it('Calcular saldo da conta', () => {
+	it.only('Calcular saldo da conta', () => {
+		// Consultando o saldo da conta antes da movimentação
+		cy.request({
+			method: 'GET',
+			url: '/saldo',
+			headers: { Authorization: `JWT ${token}` }
+		}).then(res => {
+			let saldoConta = null
+			res.body.forEach(c => {
+				if (c.conta === 'Conta para saldo') saldoConta = c.saldo
+			})
+			expect(saldoConta).to.be.equal('534.00')
+		})
 
+		//Localizando a movimentação a ser alterada
+		cy.request({
+			method: 'GET',
+			url: '/transacoes',
+			headers: { Authorization: `JWT ${token}` },
+			qs: { descricao: 'Movimentacao 1, calculo saldo' }
+		}).then(res => {
+			//Efetuando uma movimentação para depois consulta o saldo da conta
+			cy.request({
+				method: 'PUT',
+				url: `/transacoes/${res.body[0].id}`,
+				headers: { Authorization: `JWT ${token}` },
+				body: {
+					conta_id: res.body[0].conta_id,
+					data_pagamento: Cypress.moment(res.body[0].data_pagamento).format('DD/MM/YYYY'),
+					data_transacao: Cypress.moment(res.body[0].data_transacao).format('DD/MM/YYYY'),
+					descricao: "Movimentacao 1, calculo saldo",
+					envolvido: res.body[0].envolvido,
+					status: true,
+					valor: res.body[0].valor
+				}
+			}).its('status').should('be.equal', 200)
+		})
+		// Consultando o saldo da conta após a movimentação
+		cy.request({
+			method: 'GET',
+			url: '/saldo',
+			headers: { Authorization: `JWT ${token}` }
+		}).then(res => {
+			let saldoConta = null
+			res.body.forEach(c => {
+				if (c.conta === 'Conta para saldo') saldoConta = c.saldo
+			})
+			expect(saldoConta).to.be.equal('4034.00')
+		})
 	})
+
 
 	it('Excluindo a movimentação com sucesso', () => {
 
 	})
-
 })
+
+
